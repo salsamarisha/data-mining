6c3ee78a08a8712d6f4765c6c0edbe18c23d1b88
Marisha Salsabila <salsamarisha98@gmail.com>
2017-10-25 19:19:57 +0700

    Add files via upload

---
+++ iris/iris.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.cross_validation import train_test_split
+
+
+# In[2]:
+
+train=pd.read_csv('train.csv')
+test=pd.read_csv('test.csv')
+
+
+# In[ ]:
+
+train
+
+
+# In[ ]:
+
+
+
